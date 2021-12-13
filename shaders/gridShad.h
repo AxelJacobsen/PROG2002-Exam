efe0cf61caa4c45251c4daa6dffcbb23af6b0f9a
Axel Jacobsen <aejacobs@stud.ntnu.no>
2021-12-13 17:13:14 +0100

    added gridshader

---
+++ shaders/gridShad.h
@@ -0,0 +1,48 @@
+#ifndef __GRIDSHAD_H_
+#define __GRIDSHAD_H_
+
+#include <string>
+
+static const std::string gridVertexShaderSrc = R"(
+#version 430 core
+
+/** Inputs */
+in vec3 gPosition;
+in vec2 gTexcoord;
+
+/** Uniform */
+uniform mat4 view;
+uniform mat4 projection;
+
+/** Outputs */
+out vec2 gridTexcoord;
+
+void main()
+{
+//We multiply our matrices with our position to change the positions of vertices to their final destinations.
+gridTexcoord = gTexcoord;
+gl_Position =  projection * view * vec4(gPosition, 1.0f); //
+}
+)";
+
+static const std::string gridFragmentShaderSrc = R"(
+#version 430 core
+
+/** Inputs */
+in vec2 gridTexcoord;
+
+/** Outputs */
+out vec4 color;
+
+uniform sampler2D u_gridTexture;
+uniform vec4 u_Color;
+
+void main()
+{
+	vec4 textColorG = texture(u_gridTexture, gridTexcoord);
+	color = textColorG;
+}
+)";
+
+
+#endif // __GRIDSHAD_H_
